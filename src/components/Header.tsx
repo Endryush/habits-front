6af4a0a70a59cabbad883a36a480edc5f1893f56
Endryush <endryush@gmail.com>
2023-01-21 09:36:22 -0300

    final

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,43 +1,32 @@
-import { Plus, X } from "phosphor-react";
-import * as Dialog from '@radix-ui/react-dialog';
+import { View, TouchableOpacity, Text } from "react-native";
+import { Feather } from '@expo/vector-icons';
+import colors from 'tailwindcss/colors';
+import { useNavigation } from "@react-navigation/native";
 
-import LogoImage from '../assets/logo.svg'
-import { NewHabitForm } from "./NewHabitForm";
+import Logo from '../assets/logo.svg';
 
 export function Header() {
+  const { navigate } = useNavigation()
 
   return (
-    <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
-      <img src={LogoImage} alt="" />
+    <View className="w-full flex-row items-center justify-between">
+      <Logo />
 
-      <Dialog.Root>
-        <Dialog.Trigger
-          type="button"
-          className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300"
-        >
-          <Plus size={20} className="text-violet-500" />
-          Novo hábito
-        </Dialog.Trigger>
+      <TouchableOpacity 
+        activeOpacity={0.7}
+        className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center" 
+        onPress={() => navigate('new')} 
+      >
+        <Feather 
+          name="plus"
+          color={colors.violet[500]}
+          size={20}
+        />
 
-        <Dialog.Portal>
-          <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
-
-          <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <Dialog.Close className="absolute right-6 top-6 text-zinc-400 hover:text-zinc-200">
-              <X size={24} aria-label="Fechar"  />
-            </Dialog.Close>
-
-            <Dialog.Title className="text-3xl leading-tight font-extrabold">
-              Criar bábito
-            </Dialog.Title>
-
-            <NewHabitForm />
-          </Dialog.Content>
-        </Dialog.Portal>
-
-      </Dialog.Root>
-
-      
-    </div>
-  );
+        <Text className="text-white ml-3 font-semibold text-base">
+          Novo
+        </Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
